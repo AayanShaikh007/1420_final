a184a0b733631352d595e9619f36d495afeef9df
ReiReyes <reinaldoreyes02@gmail.com>
2026-03-19 21:21:26 -0400

    Merging

--- src/main/java/com/example/_420_final/Launcher.java
+++ src/main/java/com/example/_420_final/Launcher.java
@@ -7,6 +7,8 @@ public class Launcher {
     public static void main(String[] args) {
 
         FileManagement.loadFiles("/Files/users.csv", "/Files/events.csv", "/Files/bookings.csv");
+        Runtime.getRuntime().addShutdownHook(new Thread(FileManagement::saveAll));
+
         Application.launch(HelloApplication.class, args);
     }
 }
